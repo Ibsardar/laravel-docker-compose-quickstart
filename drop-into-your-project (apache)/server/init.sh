d0554de834b245ae9c6b15abb893753c1637e5c5
Ibrahim Sardar <isardar@funaiservice.com>
2021-01-28 22:57:09 -0500

    Added NGINX support

--- drop-into-your-project (apache)/server/init.sh
+++ drop-into-your-project (apache)/server/init.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 cd /var/www/html/laravel-project
+
+# UNCOMMENT FOR INITIAL DEPLOYMENT ONLY:
 #php artisan migrate:fresh --seed
+
 chgrp -R www-data storage bootstrap/cache
 chmod -R ug+rwx storage bootstrap/cache
+
+# UNCOMMENT FOR PRODUCTION:
+#composer install --optimize-autoloader --no-dev
+
 php artisan cache:clear
 php artisan config:clear
 
